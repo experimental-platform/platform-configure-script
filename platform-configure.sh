aef7cb92000aad8203cf09ad9f5c9337896db84d
Hinnerk Haardt <haardt@information-control.de>
2015-08-17 15:34:58 +0200

    Even more debug messages.

--- platform-configure.sh
+++ platform-configure.sh
@@ -37,17 +37,17 @@ function print_usage() {
 
 function download_and_verify_image() {
   local image=$1
-  echo -e "\tTagging image..."
+  echo -e "\tINSTALLER: Tagging old image"
   $DOCKER tag -f $image "$image-previous" 2>/dev/null || true # do not fail, this is just for backup reason
-  echo -e "\tPulling Image"
+  echo -e "\tINSTALLER: Pulling Image"
   $DOCKER pull $image
-  echo -e "\tValidating all layers"
+  echo -e "\tINSTALLER: Validating all layers"
   for layer in $(docker history --no-trunc $image | tail -n +2 | awk '{ print $1 }'); do
     # This is the most stupid way to check if all layer were downloaded correctly.
     # But it is the fastest one. The docker save command takes about 30 Minutes for all images,
     # even with output piped to /dev/null.
     if [[ ! -e /var/lib/docker/overlay/$layer || ! -e /var/lib/docker/graph/$layer ]]; then
-      echo "Layer '$layer' of '$image' missing. Switching to previous version (if there was one)."
+      echo "INSTALLER: Layer '$layer' of '$image' missing. Switching to previous version (if there was one)."
       $DOCKER tag -f "$image-previous" $image 2>/dev/null
       exit 1
     fi
@@ -94,7 +94,7 @@ if [ "$DEBUG" = true ]; then
 fi
 
 if [ "$(id -u)" != "0" ]; then
-  echo "Can not run without root pemissions."
+  echo "INSTALLER: Can not run without root permissions."
   exit 2
 fi
 
@@ -103,17 +103,19 @@ mkdir -p $IMAGE_STATE_DIR
 if [[ -z "${CHANNEL}" ]]; then
   if [ -e ${CHANNEL_FILE} ]; then
     CHANNEL=$(cat ${CHANNEL_FILE})
-    echo "Using channel '${CHANNEL}' from ${CHANNEL_FILE}."
+    echo "INSTALLER: Using channel '${CHANNEL}' from ${CHANNEL_FILE}."
   else
     CHANNEL=stable
-    echo "No channel given. Using '${CHANNEL}' (default channel)."
+    echo "INSTALLER: No channel given. Using '${CHANNEL}' (default channel)."
   fi
 else
-  echo "Using '${CHANNEL}' from the command line."
+  echo "INSTALLER: Using '${CHANNEL}' from the command line."
 fi
 
+echo "INSTALLER: FETCHING configure:${CHANNEL}"
 download_and_verify_image $REGISTRY/configure:${CHANNEL}
 
+echo "INSTALLER: Cleaning up and installing configuration"
 # clean up running update task!
 $DOCKER kill $CONTAINER_NAME 2>/dev/null || true
 $DOCKER rm $CONTAINER_NAME 2>/dev/null || true
@@ -123,11 +125,14 @@ $DOCKER run --rm --name=$CONTAINER_NAME \
             --volume=/opt/bin/:/host-bin/ \
             $REGISTRY/configure:$CHANNEL
 
+echo "INSTALLER: Enabling systemd-networkd-wait-online.service"
 # Make sure we're actually waiting for the network if it's required.
 systemctl enable systemd-networkd-wait-online.service
 
+echo "INSTALLER: Enabling services."
 find /etc/systemd/system -maxdepth 1 ! -name "*.sh" -type f -exec systemctl enable {} +
 # .path files need to be started!
+echo "INSTALLER: Starting .path files."
 find /etc/systemd/system -maxdepth 1 -name "*.path" -type f | xargs basename -a | xargs systemctl restart
 
 mkdir -p $(dirname $CHANNEL_FILE)
@@ -140,35 +145,37 @@ echo $CHANNEL > $CHANNEL_FILE
 # When using a feature branch most images come from the development channel:
 available_channels="development alpha beta stable"
 if [[ ! ${available_channels} =~ ${CHANNEL} ]]; then
-  echo "We're on feature channel '${CHANNEL}'"
+  echo "INSTALLER: We're on feature channel '${CHANNEL}'"
   CHANNEL=development
 fi
 
 # prefetch buildstep. so the first deployment doesn't have to fetch it.
+echo "INSTALLER: FETCHING experimentalplatform/buildstep:latest"
 download_and_verify_image experimentalplatform/buildstep:latest
 # required in init-protonet.service. BOOT FAILS IF THIS ISN'T PRESENT!
+echo "INSTALLER: FETCHING ibuildthecloud/systemd-docker"
 download_and_verify_image ibuildthecloud/systemd-docker
 # Complex regexp to find all images names in all service files
 IMAGES=$(grep -hor -i "$REGISTRY\/[a-zA-Z0-9:_-]\+\s\?" /etc/systemd/system/*.service)
 for IMAGE in $IMAGES; do
   # Doesn't work on buildstep as it is build w/ tag "latest" only.
   if [[ ! ${IMAGE} =~ "experimentalplatform/buildstep" ]]; then
-    echo "FETCHING ${IMAGE}"
+    echo "INSTALLER: FETCHING ${IMAGE}"
     download_and_verify_image $IMAGE
   else
-    echo "NOT FETCHING ${IMAGE} ;)"
+    echo "INSTALLER: NOT FETCHING ${IMAGE} ;)"
   fi
 done
-echo "DONE FETCHING ALL IMAGES."
+echo "INSTALLER: DONE FETCHING ALL IMAGES."
 
 if [ "$RELOAD" = true ]; then
-  echo "Reloading systemctl after update."
+  echo "INSTALLER: Reloading systemctl after update."
   systemctl restart init-protonet.service
   exit 0
 fi
 
 if [ "$REBOOT" = true ]; then
-  echo "Rebooting after update."
+  echo "INSTALLER: Rebooting after update."
   shutdown --reboot now "Rebooting system for experimental-platform update."
   exit 0
 fi
