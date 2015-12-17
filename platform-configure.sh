bf2b01717d95d7e575915e73754ba06c0e679850
Hinnerk Haardt <haardt@information-control.de>
2015-12-17 13:32:45 +0100

    Try and get all images.

--- platform-configure.sh
+++ platform-configure.sh
@@ -201,7 +201,7 @@ function install_platform() {
   # prefetch buildstep. so the first deployment doesn't have to fetch it.
   download_and_verify_image experimentalplatform/buildstep:herokuish
   # Complex regexp to find all images names in all service files
-  IMAGES=$(grep -hor -i "$REGISTRY\/[a-zA-Z0-9:_-]\+\s\?" /etc/systemd/system/*.service)
+  IMAGES=$(awk '!/^\s*[a-zA-Z0-9]+=|\[|^#|^\s*$|^\s*\-|^\s*bundle/ {print $1}'  /etc/systemd/system/*.service)
   for IMAGE in $IMAGES; do
     # Doesn't work on buildstep as it is build w/ tag "latest" only.
     if [[ ! ${IMAGE} =~ "experimentalplatform/buildstep" ]]; then
